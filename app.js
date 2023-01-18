9bd6d26c07bef1a606d7624d23c2405fc5bddbac
sumon-hossain <sumonengr.96@gmail.com>
2023-01-18 23:40:04 +0600

    WIP

--- app.js
+++ app.js
@@ -1,32 +1,45 @@
-// external import
+// external imports
 const express = require("express");
+const http = require("http");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const path = require("path");
 const cookieParser = require("cookie-parser");
+const moment = require("moment");
 
-// internal import
+// internal imports
 const loginRouter = require("./router/loginRouter");
-const usersRouter = require("./router/userRouter");
+const usersRouter = require("./router/usersRouter");
 const inboxRouter = require("./router/inboxRouter");
+
+// internal imports
 const {
   notFoundHandler,
   errorHandler,
 } = require("./middlewares/common/errorHandler");
 
 const app = express();
+const server = http.createServer(app);
 dotenv.config();
+const port = process.env.PORT || 8090;
+// socket creation
+const io = require("socket.io")(server);
+global.io = io;
 
-const port = process.env.PORT || 7070;
+// set comment as app locals
+app.locals.moment = moment;
 
 // database connection
 mongoose.set("strictQuery", false);
 mongoose
-  .connect(process.env.MONGO_URL, {})
-  .then(() => console.log("database connected successfully!"))
-  .catch((error) => console.log(error));
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("database connection successful!"))
+  .catch((err) => console.log(err));
 
-//request parsers
+// request parsers
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -39,16 +52,17 @@ app.use(express.static(path.join(__dirname, "public")));
 // parse cookies
 app.use(cookieParser(process.env.COOKIE_SECRETE));
 
-// routin page
+// routing setup
 app.use("/", loginRouter);
 app.use("/users", usersRouter);
 app.use("/inbox", inboxRouter);
 
-// 404 not found error handler
+// 404 not found handler
 app.use(notFoundHandler);
+
 // common error handler
 app.use(errorHandler);
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+server.listen(port, () => {
+  console.log(`app listening to port http://localhost:${port}`);
 });
--- router/usersRouter.js
+++ router/usersRouter.js
@@ -1,21 +1,28 @@
+// external imports
 const express = require("express");
+const { check } = require("express-validator");
+
+// internal imports
 const {
   getUsers,
   addUser,
   removeUser,
 } = require("../controller/usersController");
-const { checkLogin } = require("../middlewares/common/checkLogin");
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
 const avatarUpload = require("../middlewares/users/avatarUpload");
 const {
   addUserValidators,
   addUserValidationHandler,
-} = require("../middlewares/users/usersValidators");
+} = require("../middlewares/users/userValidators");
+
+const { checkLogin } = require("../middlewares/common/checkLogin");
+
 const router = express.Router();
 
+// users page
 router.get("/", decorateHtmlResponse("Users"), checkLogin, getUsers);
 
-// add users
+// add user
 router.post(
   "/",
   checkLogin,
